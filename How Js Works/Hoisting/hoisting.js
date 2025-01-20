e1d0677b8d9ba19e6c3457ad1bdd051fe794f1fe
Nikhil Pandey <91266675+Nikhil70700@users.noreply.github.com>
2025-01-20 22:00:20 +0530

    Hoisting In js

--- How Js Works/Hoisting/hoisting.js
+++ How Js Works/Hoisting/hoisting.js
@@ -3,7 +3,7 @@
 //todo When a function declaration is hoisted, its entire definition (including the body) is moved to the top of its containing scope during the creation phase. This means that you can call the function before it's actually declared in the code, and it will still work as expected.
 
 let myVar = 10;
-const greet = () => {
+function greet () {
   console.log("Welcome, If you are reading this, Don't forget you are awesome");
 };
 
