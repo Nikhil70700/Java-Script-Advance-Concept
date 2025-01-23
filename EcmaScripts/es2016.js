76e94654df165bb4e58f92027537efea6fa05b56
Nikhil Pandey <91266675+Nikhil70700@users.noreply.github.com>
2025-01-24 03:20:45 +0530

    Ecma Script 2016

--- EcmaScripts/es2016.js
+++ EcmaScripts/es2016.js
@@ -11,18 +11,23 @@
 //* syntax
 // base_value ** exponent_value;
 
+
 //? Basic usage:
-let base = 2;
-let exponent = 3;
-console.log("using Math.pow()", Math.pow(base, exponent));
-console.log("using exponentiation operator", base ** exponent);
+// let base = 2;
+// let exponent = 3;
+// console.log("using Math.pow()", Math.pow(base, exponent));
+// console.log("using exponentiation operator", base ** exponent);
 
 //? In expressions:
 //! calculates the area of a circle with a radius of 5 units.
-// let area = Math.PI * 5 ** 2;
-// console.log(area.toFixed(2));
+let area = Math.PI * 5 ** 2;
+// console.log(area);
+console.log(area.toFixed(2));
 
 //* ==============================
 //*  Array.includes()
 //* ===============================
-//? we have already cover in our world best JS course part-1 at 7:05:01 timeline
+
+
+let arr=[1,3,4,5,68,8]
+console.log(arr.includes(2));
